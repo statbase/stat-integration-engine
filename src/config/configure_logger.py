d2e2e10206d0d6a9ae06e407b1b094ea5d831b45
Fredrik Olofsson <ksfredrikolofsson@gmail.com>
2023-10-05 13:24:47 +0200

    Update configure_logger.py
    Need guidance if we call configure_logger.py from main.py or when we get the logger

--- src/config/configure_logger.py
+++ src/config/configure_logger.py
@@ -3,12 +3,13 @@ import sys
 
 def configure_logging():
     logging.basicConfig(
-        level=logging.NOTSET,
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] %(filename)s: %(message)s",
         handlers=[
             logging.StreamHandler(sys.stdout)  # Logging to terminal
         ]
     )
 
 def get_logger(name: str):
+    configure_logging() 
     return logging.getLogger(name)
